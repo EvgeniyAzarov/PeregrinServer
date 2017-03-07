ecc840b9ed162d00a225865fbdc9b4eaf07ecd15
Evgeniy <azarov.evg.ua@gmail.com>
2017-03-07 22:34:46 +0200

    Add request-response principle

--- src/PeregrinServer.java
+++ src/PeregrinServer.java
@@ -3,6 +3,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class PeregrinServer {
 
@@ -14,28 +15,36 @@ public class PeregrinServer {
         ObjectInputStream inputStream;
         ObjectOutputStream outputStream;
 
-        String buffer = "";
+        ArrayList<String> dataStorage = new ArrayList<>();
 
         while (true) {
             Socket socket = server.accept();
 
             inputStream = new ObjectInputStream(socket.getInputStream());
-            String message = (String) inputStream.readObject();
-            System.out.println("Message Received: " + message);
-            buffer = message;
+            String request = (String) inputStream.readObject();
 
-            outputStream = new ObjectOutputStream(socket.getOutputStream());
-            outputStream.writeObject(buffer);
+            try {
+                if (request.substring(0, 4).equals("POST")) {
+                    dataStorage.add(request.substring(5, request.length()));
+                } else if (request.substring(0, 3).equals("GET")) {
+
+                    int lastGetMessageIndex = Integer.valueOf(request.substring(4, request.length()));
+
+                    outputStream = new ObjectOutputStream(socket.getOutputStream());
+
+                    for (int i = lastGetMessageIndex + 1; i < dataStorage.size(); i++) {
+                        outputStream.writeObject(dataStorage.get(i));
+                    }
+                    outputStream.writeObject("END");
+
+                    outputStream.close();
+                }
+            } catch (Exception e) {
+                System.out.println("Ignored incorrect request");
+            }
 
             inputStream.close();
-            outputStream.close();
             socket.close();
-
-            if (message.equals("exit")) break;
         }
-
-        System.out.println("Shutting down Socket server!!");
-
-        server.close();
     }
 }
